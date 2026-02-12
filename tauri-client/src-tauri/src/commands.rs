af215505990386808652754650981c5e54316e6b
Jim MacKenzie <protojazz@gmail.com>
2026-02-11 18:13:39 -0600

    Tarui, but it doesn't support webrtc so idk

---
+++ tauri-client/src-tauri/src/commands.rs
@@ -0,0 +1,4 @@
+#[tauri::command]
+pub fn request_media_permissions() -> bool {
+    true
+}
---
+++ tauri-client/src-tauri/src/lib.rs
@@ -0,0 +1,12 @@
+mod commands;
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_global_shortcut::Builder::default().build())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![commands::request_media_permissions])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
